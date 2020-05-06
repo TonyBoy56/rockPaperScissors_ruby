29445c275048d3e84b27c270998e0ac2a6ebca06
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-06 15:40:36 -0700

    add score tallys via user1 & user@ variables

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -3,6 +3,8 @@ class Game
   def initialize(user, computer_choice)
     @user = user
     @computer_choice = computer_choice
+    @user1_score = 0
+    @user2_score = 0
   end  
 
   def computer_choice
@@ -11,13 +13,15 @@ class Game
   
   def determineWinner
     if @user == @computer_choice
-      return "I choose #{@computer_choice} too, we tied."
+      return "I chose #{@computer_choice} too, we tied."
     elsif 
       (@user == "rock" && @computer_choice == "scissors") || 
       (@user == "scissors" && @computer_choice == "paper") ||
       (@user == "paper" && @computer_choice == "rock")
+       @user1_score += 1
     return "You won!"
   else
+    @user2_score += 1
     return "You Lost!"
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -8,20 +8,20 @@ describe ("#Game") do
 
 
 describe("#determineWinner") do 
-  it ('returns true if rock is the user input and scissors is the arugment') do 
-  game = Game.new("rock", "scissors")
-  expect(game.determineWinner()).to(eq("You won!"))
+    it ('returns true if rock is the user input and scissors is the arugment') do 
+    game = Game.new("rock", "scissors")
+    expect(game.determineWinner()).to(eq("You won!"))
   end
 
   it ('returns you lost! if user input losing choice') do 
-  game = Game.new("paper", "scissors")
-  expect(game.determineWinner()).to(eq("You Lost!"))
+    game = Game.new("paper", "scissors")
+    expect(game.determineWinner()).to(eq("You Lost!"))
   end
 
   it ('returns we tied if user input is the same as computer choice') do 
     game = Game.new("paper", "paper")
-    expect(game.determineWinner()).to(eq("I choose #{game.computer_choice} too, we tied."))
-    end
+    expect(game.determineWinner()).to(eq("I chose #{game.computer_choice} too, we tied."))
+  end
 
 end
 end
